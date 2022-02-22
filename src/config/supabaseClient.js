e67280b4ca83f47d0cd387cd6a531547bf78b888
joce1112 <180935@utags.edu.mx>
2022-02-22 01:04:41 -0600

    7 commit

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = "https://cezwupsdtqfovnchmtbv.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlend1cHNkdHFmb3ZuY2htdGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNzczNTAsImV4cCI6MTk1OTk1MzM1MH0.Cls5w0rspIQNrCEPtMZpvBi1GbMkONU_lUdWjkk-DVE"
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient("https://cezwupsdtqfovnchmtbv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlend1cHNkdHFmb3ZuY2htdGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNzczNTAsImV4cCI6MTk1OTk1MzM1MH0.Cls5w0rspIQNrCEPtMZpvBi1GbMkONU_lUdWjkk-DVE")
